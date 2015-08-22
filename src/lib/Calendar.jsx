0e0900bd4553ca620da97f33ca8f7af0d349ade3
tianxiang.wly <tianxiang.wly@alibaba-inc.com>
2015-08-22 19:33:58 +0300

    add datepicker

--- src/lib/Calendar.jsx
+++ src/lib/Calendar.jsx
@@ -1,56 +1,120 @@
 import React from 'react';
+import ReactMixin from 'react-mixin';
+import EventMixin from 'react-as-event-mixin';
 
 import DatePicker from './DatePicker';
 
 import {
-	timeFormat
+  getCalendar,
+  isDate,
+  equal,
+  obj2date,
+  date2obj,
+  toDate,
+  inRange,
+  timeFormat
 } from './util';
 
-export default class Calendar extends React.Component {
+class Calendar extends React.Component {
 	static propTypes = {
-		value: React.PropTypes.object,
+		year: React.PropTypes.number,
+		month: React.PropTypes.number,
+		value: React.PropTypes.oneOfType([
+      React.PropTypes.string,
+      React.PropTypes.object
+    ]),
+		showPanel: React.PropTypes.bool,
 		format: React.PropTypes.string,
-		onChange: React.PropTypes.func
-	},
-	static defaultProps = {
-		value: null,
-		format: 'yyyy-MM-dd',
-		onChange: () => {}
-	},
-	constructor() {
-		super();
-		this.state = {
-			focus: false,
-			value: null
+		blacklist: React.PropTypes.arrayOf(React.PropTypes.object),
+		onChange: React.PropTypes.func,
+		onPickerShow: React.PropTypes.func,
+		onPickerHide: React.PropTypes.func,
+		onYearChange: React.PropTypes.func,
+		onMonthChange: React.PropTypes.func
+	}
+	static defaultProps = (function() {
+		var today = date2obj(new Date());
+		var noop = () => {};
+		return {
+			value: null,
+			format: 'yyyy-MM-dd',
+			onChange: noop,
+			onPickerShow: noop,
+			onPickerHide: noop,
+			onYearChange: noop,
+			onMonthChange: noop,
+			year: null,
+			month: null,
+			showPanel: false
 		};
-		this.handleFocus = this.handleFocus.bind(this);
-		this.handleBlur = this.handleBlur.bind(this);
-		this.handlePickerSelect = this.handlePickerSelect.bind(this);
+	} ())
+	constructor(props) {
+		super();
+		var today = date2obj(new Date());
+		var value = toDate(props.value);
+		var objValue = date2obj(value);
+		this.state = {
+			showPanel: props.showPanel,
+			value: value,
+			year: props.year || objValue.year || today.year,
+			month: props.month || objValue.month || today.month
+		};
+		this._handleFocus = this._handleFocus.bind(this);
+		this._handleSelect = this._handleSelect.bind(this);
 	}
-	handleFocus() {
-		this.setState({
-			focus: true
-		});
+	componentWillReceiveProps(nextProps) {
+		var newState = {};
+		nextProps.value && (newState.value = toDate(nextProps.value));
+		nextProps.year && (newState.year = toDate(nextProps.year));
+		nextProps.month && (newState.month = toDate(nextProps.month));
+		nextProps.showPanel && (newState.showPanel = toDate(nextProps.showPanel));
+		this.setState(newState);
 	}
-	handleBlur() {
-		this.setState({
-			focus: false
-		});
+	componentDidMount() {
+		var container = React.findDOMNode(this);
+		document.body.addEventListener('click', (e) => {
+			if(!container.contains(e.target)) {
+				this.setState({
+					showPanel: false
+				});
+				this.fireAll('panelHide', this);
+			}
+		}, false);
 	}
-	handlePickerSelect(year, month, day) {
+	_handleFocus() {
 		this.setState({
-			focus: false,
-			value: new Date(year, month - 1, day)
+			showPanel: true
 		});
+		this.fireAll('panelShow', this);
+	}
+	_handleSelect(date) {
+		this.setState({
+			value: date
+		});
+		this.fireAll('change', date);
 	}
 	render() {
 		return (
 			<span className="react-as-calendar">
-				<input value={this.state.value} onBlur={this.handleBlur} onFocus={this.handleFocus}></input>
+				<input value={timeFormat(this.state.value, this.props.format)} onFocus={this._handleFocus}></input>
 				{
-					this.state.focus ? <DatePicker year={} onSelect={this.handlePickerSelect}/> : ''
+					this.state.showPanel && (
+						<div className="panel">
+
+							<DatePicker
+								year={this.state.year}
+								month={this.state.month}
+								blacklist={this.props.blacklist}
+								value={this.state.value}
+								onChange={this._handleSelect}/>
+						</div>
+					)
 				}
 			</span>
 		);
 	}
 }
+
+ReactMixin(Calendar.prototype, EventMixin);
+
+export default Calendar;
--- src/lib/util.jsx
+++ src/lib/util.jsx
@@ -1,4 +1,5 @@
 function timeFormat(date, format) {
+  if(!date) return '';
   var o = {
     "M+": date.getMonth() + 1, //month
     "d+": date.getDate(), //day
@@ -12,7 +13,9 @@ function timeFormat(date, format) {
     format = format.replace(RegExp.$1, (date.getFullYear() + "").substr(4 - RegExp.$1.length));
   for (var k in o)
     if (new RegExp("(" + k + ")").test(format))
-      format = format.replace(RegExp.$1, RegExp.$1.length === 1 ? o[k] : ("00" + o[k]).substr(("" + o[k]).length));
+      format = format.replace(RegExp.$1, RegExp.$1.length === 1
+        ? o[k]
+        : ("00" + o[k]).substr(("" + o[k]).length));
   return format;
 }
 
@@ -27,11 +30,16 @@ function getMonthLen(year, month) {
   return nextMonth.getDate();
 }
 
-function getMonthDetail(year, month) {
+function getCalendar(year, month) {
   var monthLen = getMonthLen(year, month);
   var firstDay = getFirstDay(year, month);
-  var list = [[]];
-  var i, cur, row, col;
+  var list = [
+    []
+  ];
+  var i,
+    cur,
+    row,
+    col;
   for (i = firstDay; i--;) {
     list[0].push('');
   }
@@ -43,13 +51,63 @@ function getMonthDetail(year, month) {
     list[row].push(i);
   }
   var lastRow = list[row];
-  for(i = 7 - lastRow.length;i--;) {
+  for (i = 7 - lastRow.length; i--;) {
     lastRow.push('');
   }
   return list;
 }
 
+function isDate(v) {
+  return Object.prototype.toString.call(v).toLowerCase() === '[object date]';
+}
+
+function equal(a, b) {
+  a = isDate(a) ? date2obj(a) : a;
+  b = isDate(b) ? date2obj(b) : b;
+  return (a.year === b.year) && (a.month === b.month) && (a.day === b.day);
+}
+
+function obj2date(obj) {
+  if(!obj) return null;
+  return new Date(`${obj.year}-${obj.month}-${obj.day}`);
+}
+
+function date2obj(date) {
+  if(!date) return {};
+  return {
+    year: date.getFullYear(),
+    month: date.getMonth() + 1,
+    day: date.getDate()
+  };
+}
+
+function toDate(v) {
+  if(!v) return null;
+  if(typeof v === 'string')
+    return new Date(v);
+  else if(isDate(v))
+    return v;
+  else if(v.year && v.month && v.day)
+    return obj2date(v);
+  else
+    return null;
+}
+
+function inRange(date, from, to) {
+  date = toDate(date).getTime();
+  var can = true;
+  from && (can = can && date >= from.getTime());
+  to && (can = can && date < to.getTime());
+  return can;
+}
+
 export default {
-  timeFormat,
-  getMonthDetail
+  getCalendar,
+  isDate,
+  equal,
+  obj2date,
+  date2obj,
+  toDate,
+  inRange,
+  timeFormat
 };
